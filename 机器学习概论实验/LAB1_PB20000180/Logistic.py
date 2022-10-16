ea94e03004e70eecc83720be10f9590da0afbbe9
liuly <me@liuly.moe>
2022-10-16 16:20:47 +0800

    update ml lab1

--- 机器学习概论实验/LAB1_PB20000180/Logistic.py
+++ 机器学习概论实验/LAB1_PB20000180/Logistic.py
@@ -1,5 +1,7 @@
 import numpy as np
 from math import e
+
+
 class LogisticRegression:
 
     def __init__(self, penalty="l2", gamma=0.0):
@@ -8,10 +10,12 @@ class LogisticRegression:
         assert penalty in ["l2", "l1", ""], err_msg
         self.theta = None
         if penalty == 'l1':
-            self.penalty_cost = lambda theta: gamma * np.sum(np.absolute(theta))
+            self.penalty_cost = lambda theta: gamma * \
+                np.sum(np.absolute(theta))
             self.penalty_learning = lambda theta: gamma * np.sign(theta)
         elif penalty == 'l2':
-            self.penalty_cost = lambda theta: gamma / 2 * (theta * theta.T).item()
+            self.penalty_cost = lambda theta: gamma / \
+                2 * np.sum(np.square(theta))
             self.penalty_learning = lambda theta: gamma * theta
         else:
             self.penalty_cost = lambda theta: 0
@@ -26,25 +30,27 @@ class LogisticRegression:
         Fit the regression coefficients via gradient descent or other methods 
         """
         m, n = X.shape
-        theta = np.matrix(np.array([0.0] * n))
+        theta = np.matrix(np.zeros(n))
         costs = []
-        
+
         for _ in range(int(max_iter)):
-            learned = lr * ((self.sigmoid(X * theta.T) - y).T * X / m + self.penalty_learning(theta))
+            learned = lr * ((self.sigmoid(X @ theta.T) - y).T @
+                            X / m + self.penalty_learning(theta))
             if np.max(np.absolute(learned)) < tol:
                 break
             theta -= learned
 
-            y_pred = self.sigmoid(X * theta.T)
-            cost_value = -np.sum(y.T * np.log(y_pred) + (1 - y).T * np.log(1 - y_pred)) / m + self.penalty_cost(theta)
+            y_pred = self.sigmoid(X @ theta.T)
+            cost_value = -np.sum(y.T @ np.log(y_pred) + (1 - y).T @
+                                 np.log(1 - y_pred)) / m + self.penalty_cost(theta)
             costs.append(cost_value)
 
         self.theta = theta
         return theta, costs
-        
+
     def predict(self, X):
         """
         Use the trained model to generate prediction probabilities on a new
         collection of data points.
         """
-        return self.sigmoid(X * self.theta.T)
+        return self.sigmoid(X @ self.theta.T)
